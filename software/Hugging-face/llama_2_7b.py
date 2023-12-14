793ede36fb66440624532056e9085fb91f1be22d
Zhuohao-Li <edith.zhuohao.li@gmail.com>
2023-12-13 23:36:50 -0800

    llamma-7b

--- software/Hugging-face/llama_2_7b.py
+++ software/Hugging-face/llama_2_7b.py
@@ -1,13 +1,9 @@
-'''
-Author: Garrick
-LastEditTime: 2023-12-10 20:31:17
-FilePath: /ecem202a_project_23fall/software/Hugging-face/llama_2_7b.py
-zhuohaol@ucla.edu
-Copyright (c) 2023 by Zhuohao Li, All Rights Reserved. 
-'''
+
+
 from transformers import AutoTokenizer
 import transformers
 import torch
+import time
 
 model = "meta-llama/Llama-2-7b-chat-hf"
 
@@ -19,13 +15,26 @@ pipeline = transformers.pipeline(
     device_map="auto",
 )
 
+start_time = time.time()
+
+
+# start of inference
 sequences = pipeline(
-    'Who is the CEO of Tesla?\n',  # write prompt here
+    'Solve -179*q = -190*q - 22 for q. Solve -118525*z = -118565*z + 2720 for z.\n',
     do_sample=True,
     top_k=10,
     num_return_sequences=1,
     eos_token_id=tokenizer.eos_token_id,
     max_length=200,
 )
+# end of inference
+
+end_time = time.time()
+
+# 计算运行时间
+elapsed_time = end_time - start_time
+print(f"Inference took {elapsed_time} seconds.")
+
+
 for seq in sequences:
     print(f"Result: {seq['generated_text']}")
